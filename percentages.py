993a2930227fc0410f014fdb1cadc9b065b14dae
scaryvoid <corydavis900@msn.com>
2021-12-15 09:51:06 -0700

    Added percentage calculator.

---
+++ percentages.py
@@ -0,0 +1,26 @@
+#!/bin/env python3
+
+# print percentages of given amount
+
+import argparse, sys
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Look at depths of dips.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('initialamt', type=float, help='Amount to divide up.')
+    parser.add_argument('percentages', metavar='<n>', type=float, nargs=argparse.REMAINDER, help='Percentages to divide initialamt into.')
+    args = parser.parse_args()
+    args.percentages.sort(reverse=True)
+    
+    # may not want this bit, make optional?
+    if sum(args.percentages) != 100:
+        print(f'Error: Sum of percentages ({sum(args.percentages)}) is not 100')
+        sys.exit()
+    
+    for p in args.percentages:
+        print(f'{p}% of args.initialamt = {args.initialamt * (float(p) / 100):.02f}')
+    
+
+if __name__ == "__main__":
+    main()
